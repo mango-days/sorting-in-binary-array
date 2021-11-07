4b9207354fbb9dab7427c380ce4d87eeac253bc9
mango-days <77330707+mango-days@users.noreply.github.com>
2021-11-07 18:39:57 +0530

    Update binary_array.py

--- binary_array.py
+++ binary_array.py
@@ -1,11 +1,11 @@
 binary_array=[1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0]
-i=0
-j=len(binary_array)-1
-while (i<=j):
-    if binary_array[i]==1:
-        if binary_array[j]==0:
-            binary_array[i]=0
-            binary_array[j]=1
-        else: j-=1
-    else: i+=1
+starting_index=0
+ending_index=len(binary_array)-1
+while (starting_index<=ending_index):
+    if binary_array[starting_index]==1:
+        if binary_array[ending_index]==0:
+            binary_array[starting_index]=0
+            binary_array[ending_index]=1
+        else: ending_index-=1
+    else: starting_index+=1
 print (binary_array)
